69256e75ffc4fa07667866d3942e1c186e2c1365
jurajkocan <jurajkocan@windowslive.com>
2017-11-18 15:32:38 +0100

    api types

---
+++ lib/ApiTypes/Request.ts
@@ -0,0 +1,6 @@
+export namespace AppRequest {
+    export interface LoginUser {
+        email: string
+        password: string
+    }
+}
---
+++ lib/ApiTypes/Response.ts
@@ -0,0 +1,10 @@
+export namespace AppResponse {
+    export interface LoginUser {
+        email: string
+        token: string
+    }
+
+    export interface User {
+        email: string
+    }
+}
